1067436e67f6f1b3a93720ccfb93796c9c996bcb
Redoy Al Hasan <mdredoyhasan49@gmail.com>
2025-07-15 13:00:22 +0600

    add RootLayout with Navbar, Footer, and Outlet structure

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../components/shared/Navbar';
+import Footer from '../components/shared/Footer';
 
 const RootLayout = () => {
   return (
-    <div>
-      Root Layout
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
