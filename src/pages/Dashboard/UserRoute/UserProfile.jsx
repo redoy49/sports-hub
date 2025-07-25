1cbba0a12a9027a0080e8544e6a9f191e391f675
Redoy Al Hasan <mdredoyhasan49@gmail.com>
2025-07-25 09:39:08 +0600

    Polish UserProfile in dashboard for user

--- src/pages/Dashboard/UserRoute/UserProfile.jsx
+++ src/pages/Dashboard/UserRoute/UserProfile.jsx
@@ -1,15 +1,15 @@
 // components/UserProfile.jsx
-import { useQuery } from '@tanstack/react-query';
-import useAuth from '../../../hooks/useAuth';
-import LoadingSpinner from '../../../components/LoadingSpinner';
-import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import LoadingSpinner from "../../../components/LoadingSpinner";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const UserProfile = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
   const { data: userData = {}, isLoading } = useQuery({
-    queryKey: ['user', user?.email],
+    queryKey: ["user", user?.email],
     enabled: !!user?.email,
     queryFn: async () => {
       const res = await axiosSecure.get(`/users/email/${user.email}`);
@@ -17,18 +17,23 @@ const UserProfile = () => {
     },
   });
 
-  if (isLoading) return <LoadingSpinner/>;
+  if (isLoading) return <LoadingSpinner />;
 
   return (
-    <div className="max-w-md mx-auto bg-white shadow-xl rounded-xl p-6">
-      <div className="flex flex-col items-center">
-        <img src={userData.image} alt="Profile" className="w-24 h-24 rounded-full mb-4" />
-        <h2 className="text-xl font-bold">{userData.name}</h2>
-        <p className="text-gray-600">{userData.email}</p>
-        <p className="mt-4 text-sm text-gray-500">
-          Registered on: <strong>{new Date(userData.createdAt).toLocaleDateString()}</strong>
-        </p>
+    <div className="max-w-6xl mx-auto bg-white rounded-xl px-6 py-24">
+      <div className="flex flex-col items-center mb-6">
+        <img
+          src={userData.image}
+          alt="Profile"
+          className="w-24 h-24 rounded-full mb-4 ring-4 ring-blue-300"
+        />
+        <h2 className="text-2xl font-semibold">{userData.name}</h2>
+        <p className="text-gray-700">{userData.email}</p>
       </div>
+      <p className="text-sm text-gray-500 text-center">
+        Registered on:{" "}
+        <strong>{new Date(userData.createdAt).toLocaleDateString()}</strong>
+      </p>
     </div>
   );
 };
